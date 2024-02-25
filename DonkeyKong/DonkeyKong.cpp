6388202eda968b52c788d6fe8a53c3b260ff3cb5
Mats <mats@online.com>
2024-02-25 16:29:30 +0100

    update

--- DonkeyKong/DonkeyKong.cpp
+++ DonkeyKong/DonkeyKong.cpp
@@ -2,9 +2,16 @@
 #include "Game.h"
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
 	std::srand(std::time(0)); // use current time as seed for random generator
 	Game game;
 	game.run(); 
 }
+
+#ifdef _WIN32
+#include <windows.h>
+INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+	return main(__argc, __argv);
+}
+#endif
